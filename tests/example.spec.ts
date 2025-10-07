cf95b3fb65e62eb32335985d9639b7678084a388
Anneen011 <anneenjunplod@gmailcom>
2025-10-08 01:36:16 +0700

    Form testing

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,9 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+test('Basic form submit', async ({ page }) => {
+  await page.goto('http://localhost:9000')
+  await page.fill('input[label="Name"]', 'Nattha')
+  await page.fill('input[label="Email"]', 'nattha@example.com')
+  await page.fill('input[label="Password"]', '123456')
+  await page.click('button:has-text("Submit")')
+})
